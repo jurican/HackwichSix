8468773da5c704946739d826786cf3a68b513efb
jurican <97710250+jurican@users.noreply.github.com>
2022-03-02 21:26:03 -1000

    Did the Problem and Commented out the Problem set as I do not how to make both work at the same time

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -13,17 +13,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 //Part 7
                     //Part 8.2
     var myFriendsArray = ["Jamise", "Joseph", "Eric", "Jose", "Andre", "Shamar"]
+    //var myTravelArray = ["Morocco", "Luna", "Antartica", "Mars", "Orbit"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
 //Part 8.1
         return myFriendsArray.count
+        //return myTravelArray.count
     }
     
 //Part 8.3
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        //let text = myTravelArray[indexPath.row]
         let text = myFriendsArray[indexPath.row]
         cell.textLabel?.text = text
         return cell
@@ -35,4 +38,4 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view.
     }
 }
-//I don't understand enough to pull off the problem set
+//I don't understand enough to pull off the problem set and full Hackwich at the same time so I commented out the array
