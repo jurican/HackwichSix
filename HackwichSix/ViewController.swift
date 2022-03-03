7c30f50eb7f6816da407b25209aabc8d33791f38
jurican <97710250+jurican@users.noreply.github.com>
2022-03-02 21:08:40 -1000

    Hackwich6 without the Problem Set (I just don't understand it enough)

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+//Part 6
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+//Part 7
+                    //Part 8.2
+    var myFriendsArray = ["Jamise", "Joseph", "Eric", "Jose", "Andre", "Shamar"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+//Part 8.1
+        return myFriendsArray.count
+    }
+    
+//Part 8.3
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFriendsArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
+//I don't understand enough to pull off the problem set
